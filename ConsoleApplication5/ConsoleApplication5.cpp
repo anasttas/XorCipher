14ead19e323f4ba58e26c515a479a4696f95f21e
anasttas <3333333333333333222n@gmail.com>
2024-11-26 22:37:32 +0300

    second

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -4,20 +4,22 @@ using namespace std;
 
 string encode(string message, string key) {
     string result;
-    char* massMessage = new char[message.length()];;
-    char* massKey = new char[key.length()];
+    int messageLength = message.length();
+    int keyLength = key.length();
+    char* massMessage = new char[messageLength];
+    char* massKey = new char[keyLength];
 
-    for (int letterMessage = 0; letterMessage < message.length(); letterMessage++) {
+    for (int letterMessage = 0; letterMessage < messageLength; letterMessage++) {
         massMessage[letterMessage] = message[letterMessage];
     }
 
-    for (int letterKey = 0; letterKey < key.length(); letterKey++) {
+    for (int letterKey = 0; letterKey < keyLength; letterKey++) {
         massKey[letterKey] = key[letterKey];
     }
 
-    for (int letter = 0; letter < message.length(); letter++) {
+    for (int letter = 0; letter < messageLength; letter++) {
        
-        result += static_cast<char>(massMessage[letter] ^ massKey[letter % key.length()]);
+        result += static_cast<char>(massMessage[letter] ^ massKey[letter % keyLength]);
     }
 
     delete[] massMessage;
@@ -28,20 +30,22 @@ string encode(string message, string key) {
 
 string decode(string encodeMessage, string key) {
     string decodedMessage;
-    char* massEncoded = new char[encodeMessage.length()];;
-    char* massKey = new char[key.length()];
+    int encodeLength = encodeMessage.length();
+    int keyLength = key.length();
+    char* massEncoded = new char[encodeLength];;
+    char* massKey = new char[keyLength];
 
-    for (int letterMessage = 0; letterMessage < encodeMessage.length(); letterMessage++) {
+    for (int letterMessage = 0; letterMessage < encodeLength; letterMessage++) {
         massEncoded[letterMessage] = encodeMessage[letterMessage];
     }
 
-    for (int letterKey = 0; letterKey < key.length(); letterKey++) {
+    for (int letterKey = 0; letterKey < keyLength; letterKey++) {
         massKey[letterKey] = key[letterKey];
     }
 
-    for (int letter = 0; letter < encodeMessage.length(); letter++) {
+    for (int letter = 0; letter < encodeLength; letter++) {
 
-        decodedMessage += static_cast<char>(massEncoded[letter] ^ massKey[letter % key.length()]);
+        decodedMessage += static_cast<char>(massEncoded[letter] ^ massKey[letter % keyLength]);
     }
 
     delete[] massEncoded;
@@ -53,15 +57,14 @@ string decode(string encodeMessage, string key) {
 int main()
 {
     system("chcp 1251");
-    string text, key;
-
+    string text;
     cout << "Введи сообщение" << endl;
     cin >> text;
     cout << "Введи ключ" << endl;
+    string key;
     cin >> key;
     string result = encode(text, key);
-    string encodedMessage = decode(result, key);
     cout << "Шифр:" << result << endl;
+    string encodedMessage = decode(result, key);
     cout << "Дешифр:" << encodedMessage << endl;
-
 }
--- ConsoleApplication5/Xor.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-class Xor
-{
-	
-};
-
